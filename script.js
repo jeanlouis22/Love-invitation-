8279033f8320637951bf0e955aed6c304e7fb90c
jeanlouis22 <guejeanlouis5@gmail.com>
2026-08-05 00:32:17 +0000

    Enhance envelope click event with animation
    Refactor envelope click event to add opening animation and display letter after delay.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const enveloppe = document.querySelector(".enveloppe");
 
+const enveloppeScreen = document.getElementById("enveloppeScreen");
+
+const lettreScreen = document.getElementById("lettreScreen");
+
+// Quand on clique sur l'enveloppe
+
 enveloppe.addEventListener("click", function () {
-    enveloppe.classList.toggle("ouverte");
+
+    // Animation d'ouverture
+
+    enveloppe.classList.add("ouverte");
+
+    // Après l'animation, on affiche la lettre
+
+    setTimeout(function () {
+
+        enveloppeScreen.style.display = "none";
+
+        lettreScreen.classList.remove("cache");
+
+    }, 900);
+
 });
